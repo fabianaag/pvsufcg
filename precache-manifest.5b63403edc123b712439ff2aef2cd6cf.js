07b445106b2a4c5e7a793c3e6956b69d319838eb
Fabiana Alves <fabiana.gomes@ccc.ufcg.edu.br>
2020-11-13 00:35:35 -0300

    Updates

---
+++ precache-manifest.5b63403edc123b712439ff2aef2cd6cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a4d3f1903c0c84857de412556688ab",
+    "url": "/pvsufcg/index.html"
+  },
+  {
+    "revision": "1854562130d97f7f343e",
+    "url": "/pvsufcg/static/css/main.2f548e58.chunk.css"
+  },
+  {
+    "revision": "cca34dba4fb1249caa4c",
+    "url": "/pvsufcg/static/js/2.e61b82d3.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/pvsufcg/static/js/2.e61b82d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1854562130d97f7f343e",
+    "url": "/pvsufcg/static/js/main.7e4f82ad.chunk.js"
+  },
+  {
+    "revision": "d64abebbfc8a827282e7",
+    "url": "/pvsufcg/static/js/runtime-main.9819fd48.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pvsufcg/precache-manifest.49c3c394b9210935d847e0d61ede491b.js"
+  "/pvsufcg/precache-manifest.5b63403edc123b712439ff2aef2cd6cf.js"
 );
 
 self.addEventListener('message', (event) => {
